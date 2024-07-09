9418f66a25dc1eb7ba9dc4c6e8ec239f7c084ec5
Charlys .celestino <20302372-Charlyscelestin@users.noreply.replit.com>
2024-07-09 14:10:54 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+// tipo de variaveis:
+const variavel1 // Variavel constante
+let variavel2 //Variavel mutável
+var variavel1 //Variavel global obs: pouco utilizada
+// Conceito de local e Global
+const nome: "joao" //variavel constante não permite trocar valor
+  let nome1: "Batista" //variavel mutável permite trocar valor
+var nome 2: "silva" //variavel global permite trocar valor
+nome: "pedro"
+nome1: "joao"
+function funcao0 () {
+   const teste0 = "teste" //variavel constante local.
+  var teste1 = "teste1" //variavel global local.
+return teste0
+}
+ function funcao1 (){
+   const test0 = 'teste0' //variavel constante local.
+  return {teste0, teste1}
+ }
+
+// condicionais
+
